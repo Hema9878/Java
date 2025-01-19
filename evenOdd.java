b11655020bdc06f265ec626b8b1dbbe53ca3eeba
Hema9878 <130540164+Hema9878@users.noreply.github.com>
2025-01-19 12:34:37 -0800

    Add files via upload

---
+++ evenOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class evenOdd {
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+int num=scanner.nextInt();
+if(num%2==0)
+{
+System.out.println("enter a odd no");
+}
+else
+{ 
+System.out.println("enter a even no");
+}
+}
+}
