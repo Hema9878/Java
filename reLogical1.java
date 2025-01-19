410616bdec19b9bc7bbb9fb213187dc381a06b43
Hema9878 <130540164+Hema9878@users.noreply.github.com>
2025-01-19 12:33:29 -0800

    Add files via upload

---
+++ reLogical1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class reLogical1 {
+     static void sum(int num1,int num2,int num3)
+    {
+       int num1 = 10, num2 = 20, num3 = 20;
+        if ((a < b)&&(b == c)) {
+         sum= num1+num2+num3;
+            System.out.println("The sum is: "+sum);
+        }
+        else
+            System.out.println("False conditions");
+    }
+}
