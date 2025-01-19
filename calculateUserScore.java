f5b2b58ce09c53147dcac47afe1ac64f75777b26
Hema9878 <130540164+Hema9878@users.noreply.github.com>
2025-01-19 12:27:27 -0800

    Add files via upload

---
+++ calculateUserScore.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class calculateUserScore {
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+int maxScore=500;
+int userScore=423;
+float percentage=(float)userScore/maxScore*100.0f;
+System.out.println("user percentage is"+percentage);
+}
+}
+
