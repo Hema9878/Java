bdc99e08d80a8f45c72650e59364aa7f77808b32
Hema9878 <130540164+Hema9878@users.noreply.github.com>
2025-01-19 12:35:14 -0800

    Add files via upload

---
+++ posNegative.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class posNegative {
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+System.out.println("enter a no"); 
+int num=obj.nextInt();
+if(num>0)
+{
+System.out.println("it is a positive no");
+}
+else if(num<0)
+{ 
+System.out.println("it is a  negative  no");
+}
+}
+}
