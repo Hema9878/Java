4cd23b5a948b936ed0e943e6ffd00005a689f03f
Hema9878 <130540164+Hema9878@users.noreply.github.com>
2025-01-19 12:34:09 -0800

    Add files via upload

---
+++ DateFun.java
@@ -0,0 +1,12 @@
+
+import java.time.LocalDate;
+class DateFun
+{
+public static void main(String args[])
+{
+LocalDate obj=LocalDate.now();
+LocalTime obj1=LocalTime.now();
+System.out.println(obj);
+System.out.println(obj1);
+}
+}
